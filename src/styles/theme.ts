5f503ef76eab97c46077d933f1063c54b21706da
Tonidev <antoniodev2709@gmail.com>
2025-10-14 09:38:51 -0300

    Feat: Create Themes

---
+++ src/styles/theme.ts
@@ -0,0 +1,81 @@
+import type { Theme } from '@types'
+
+export const darkTheme: Theme = {
+  appBackground: '#060B26',
+  appColor: '#fff',
+  appDefaultStroke: '#21497D',
+  appLogo: 'dnc-logo-white.svg',
+  appSkeletonFrom: '#060B26',
+  appSkeletonTo: '#21497D',
+  buttons: {
+    alert: '#E80000',
+    alertColor: '#FFF',
+    alertHover: '#D80000',
+    disable: '#313649',
+    disableColor: '#6B7B8B',
+    primary: '#0C70F2',
+    primaryColor: '#FFF',
+    primaryHover: '#0061DE',
+  },
+  card: {
+    alert: '#E80000',
+    background: '#0F1535',
+    border: '#21497D',
+    success: '#008000',
+    warning: '#F7A300',
+  },
+  textInput: {
+    active: '#0F1535',
+    activeColor: '#FFF',
+    borderColor: '#21497D',
+    disable: '#282D49',
+    disableBorderColor: '#2E3F55',
+    disableColor: '#58677C',
+    placeHolderColor: '#89A7CE',
+  },
+  typographies: {
+    error: '#FF0202',
+    subtitle: '#666',
+    success: '#008000',
+  },
+}
+
+export const lightTheme: Theme = {
+  appBackground: '#FFF',
+  appColor: '#000',
+  appDefaultStroke: '#E0E0E0',
+  appLogo: 'dnc-logo-black.svg',
+  appSkeletonFrom: '#EEE',
+  appSkeletonTo: '#CCC',
+  buttons: {
+    alert: '#E80000',
+    alertColor: '#FFF',
+    alertHover: '#D80000',
+    disable: '#CCC',
+    disableColor: '#666',
+    primary: '#0C70F2',
+    primaryColor: '#FFF',
+    primaryHover: '#0061DE',
+  },
+  card: {
+    alert: '#E80000',
+    background: '#FFF',
+    border: '#E0E0E0',
+    success: '#008000',
+    warning: '#F7A300',
+  },
+  textInput: {
+    active: '#FFF',
+    activeColor: '#000',
+    borderColor: '#E0E0E0',
+    disable: '#EEE',
+    disableBorderColor: '#E0E0E0',
+    disableColor: '#666',
+    placeHolderColor: '#666',
+  },
+  typographies: {
+    error: '#FF0202',
+    subtitle: '#666',
+    success: '#008000',
+  },
+}
