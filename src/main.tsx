ff26d62410a86773f79c72086478447258cfdaa6
Tonidev <antoniodev2709@gmail.com>
2025-10-20 22:50:03 -0300

    fix: light theme never read

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { GlobalStyle, darkTheme, lightTheme } from './styles/'
+import { GlobalStyle, darkTheme } from './styles/'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
